2b662e1b3b234c665e8015d3f27bec0ad9076e64
Natcod <natnaelshetaye1@gmail.com>
2025-02-22 14:21:16 +0300

    Refactor views to improve book listing and library detail templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
+from .models import Book, Library
 
-from .models import Book
-from .models import Library
-
-# Function-based view
+# Function-based view to list all books
 def list_books(request):
+    # Fetch all books from the database
     books = Book.objects.all()
-
-    return render(request, 'list_books.html', {'books': books})
     
+    # Render the template with the list of books
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view
+# Class-based view to display details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # Add additional context if needed
         return context
