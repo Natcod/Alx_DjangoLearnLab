432deb0f7bbbea865ce789cfbdbf7dbcdfdbd7fd
Natcod <natnaelshetaye1@gmail.com>
2025-02-24 11:45:14 +0300

    update roles

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,34 +3,31 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# Create your models here.
+# Other models (Author, Book, Library, Librarian)
 class Author(models.Model):
-    name = models.CharField(max_length = 100)
-
+    name = models.CharField(max_length=100)
     def __str__(self):
         return self.name
 
 class Book(models.Model):
-    title = models.CharField(max_length= 200)
+    title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-
     def __str__(self):
         return self.title
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name='libraries')
-
     def __str__(self):
         return self.name
-    
+
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
-
-    def __stf__(self):
+    def __str__(self):
         return self.name
 
+# UserProfile Model
 class UserProfile(models.Model):
     """
     Extends the Django User model to include a role field.
@@ -40,14 +37,12 @@ class UserProfile(models.Model):
         ('Librarian', 'Librarian'), # Role for librarians
         ('Member', 'Member'),     # Role for members
     )
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)  # One-to-one relationship with User
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')  # Role field
-
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
-# Signal to automatically create a UserProfile when a new User is registered
+# Signals
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
