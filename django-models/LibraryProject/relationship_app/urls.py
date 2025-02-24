c94456f1ffd47eaaa2d88f2dd472c8a501fd3dfc
Natcod <natnaelshetaye1@gmail.com>
2025-02-24 11:19:53 +0300

    Refactor authentication URLs to use Django's built-in LoginView and LogoutView

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     # Function-based view for listing all books
@@ -8,7 +10,12 @@ urlpatterns = [
 
     # Class-based view for displaying library details
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+   # Use Django's built-in LoginView
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    
+    # Use Django's built-in LogoutView
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    
+    # Keep the custom registration view
     path('register/', views.register_view, name='register'),
 ]
