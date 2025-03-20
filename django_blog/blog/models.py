56478bc833a5b550bdef547794811c61cc53e722
Natcod <natnaelshetaye1@gmail.com>
2025-03-20 12:46:09 +0300

    Add tags field to Post model and configure Taggit in settings

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    content = models.TextField()    
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
@@ -18,3 +20,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
+    
