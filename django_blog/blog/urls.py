f592c2b41fe96029e0e6e56cba7b6e98b870bd42
Natcod <natnaelshetaye1@gmail.com>
2025-03-20 12:51:30 +0300

    Add tagging and search URLs to the blog application

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,4 +19,7 @@ urlpatterns = [
     path("post/<int:pk>/comments/new/", views.CommentCreateView.as_view(), name="comment_create"),
     path("comment/<int:pk>/update/", views.CommentUpdateView.as_view(), name="comment_update"),
     path("comment/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment_delete"),
+    # Tagging and Search URLs
+    path("tags/<str:tag>/", views.TagListView.as_view(), name="tag_list"),
+    path("search/", views.SearchView.as_view(), name="search"),
 ]
