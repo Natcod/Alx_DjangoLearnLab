3deaef3903f3db21d8d56b7903cfdadbfbf6ac95
Natcod <natnaelshetaye1@gmail.com>
2025-03-20 12:52:58 +0300

    Fix search functionality to allow partial tag matches in SearchView

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -127,7 +127,7 @@ class SearchView(ListView):
             return Post.objects.filter(
                 Q(title__icontains=query) |
                 Q(content__icontains=query) |
-                Q(tags__name__in=[query])
+                Q(tags__name__icontains=query)  
             ).distinct()
         return Post.objects.none()
     def get_context_data(self, **kwargs):
